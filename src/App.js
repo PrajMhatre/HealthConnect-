403874b1ed8adb4504e62b02b457dc22b101a152
mayurimahadik24 <mayurishamal24@gmail.com>
2024-09-28 17:20:19 +0530

    Firebase

--- src/App.js
+++ src/App.js
@@ -18,20 +18,8 @@ function App() {
         <Route path="/" element={<Login/>} />
         <Route path="/Register" element={<Registration/>} />
         <Route path="/Form" element={<SymptomsForm/>} />
-        {/* <Route path="/performance-analytics" element={<PerformanceAnalytics />} />
-        <Route path="/payroll" element={<Payroll/>}/>
-        <Route path="/roles" element={<Role/>}/>
-        <Route path="/reports" element={<Reports/>}/>
-        <Route path="/empreport" element={<EmployeeReport/>}/>
-        <Route path="/payroll-dashboard" element={<PayrollDashboard/>}/>
-        <Route path="/leave" element={<LeaveForm/>}/>
-        <Route path="/leave-history" element={<LeaveHistory/>}/>
-        <Route path="/leave-approval" element={<LeaveTable/>}/>
-        <Route path="/payroll-report" element={<PayrollReport/>}/>
-        <Route path="/directory" element={<EmployeeTable/>}/>
-        <Route path="/profile/:name" element={<EmployeeDetails/>}/>
-        <Route path="/department-management" element={<DepartmentTable/>}/> */}
-        {/* <Route path="/notification" element={<Notification/>}/> */}
+        
+       
       </Routes>
       
     </Router>
